7f1f108b3f91eb30324d37c91c62659777bfb6c9
Ooo工友ooO <r00621109@ntu.edu.tw>
2016-08-06 11:47:02 +0800

    10.練習 Angular 2 資料繫結方法：事件繫結 ( Property Binding ) + DOM 屬性繫結 ( Event Binding )

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,9 +11,19 @@ export class HeaderComponent implements OnInit {
   pageH3 = '記載著<strong>will</strong>保哥的學習過程';
   pageTitleLink = 'http://blog.miniasp.com/';
   pageIcon1Lick = 'https://www.facebook.com/will.fans';
+
+  num = 1;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  plus(event: MouseEvent){
+      console.log(event);//從這邊得到物件類別
+      //修改為必須要按著ctrl 才會加一
+      if (event.ctrlKey){
+        this.num++;
+      }
   }
 
 }
