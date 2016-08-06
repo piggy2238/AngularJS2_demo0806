882ae7f0f72f512594ec9e318ad28d33a7ba5172
Ooo工友ooO <r00621109@ntu.edu.tw>
2016-08-06 10:23:22 +0800

    03-2建立component之間的連結(在母元件使用directives註冊子元件)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header';
+
 //輸入class
 //裝飾器
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives:[HeaderComponent]
 })
 //輸出class
 export class AppComponent {
